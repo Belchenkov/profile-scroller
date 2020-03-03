8a8e4ab335a1513488ab93ad83a31fec080d2d82
Aleksey Belchenkov <u608110@gmail.com>
2020-03-03 15:31:41 +0300

    Logic of Iterator

--- js/app.js
+++ js/app.js
@@ -25,7 +25,48 @@ const data = [
     }
 ];
 
-// Profile Iterator
-function profileIterator() {
+const profiles = profileIterator(data);
 
+// Call first profile
+nextProfile();
+
+// Next Event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// Next Profile Display
+function nextProfile() {
+    const currentProfile = profiles.next().value;
+
+    if (currentProfile !== undefined) {
+        document.getElementById('profileDisplay').innerHTML = `
+        <ul class="list-group">
+            <li class="list-group-item">Name: ${currentProfile.name}</li>
+            <li class="list-group-item">Age: ${currentProfile.age}</li>
+            <li class="list-group-item">Location: ${currentProfile.location}</li>
+            <li class="list-group-item">
+                Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}
+            </li>
+        </ul>
+    `;
+    } else {
+        // No more profiles
+        window.location.reload();
+    }
+
+    document.getElementById('imageDisplay').innerHTML = `
+        <img src="${currentProfile.image}" />
+    `;
+}
+
+// Profile Iterator
+function profileIterator(profiles) {
+    let nextIndex = 0;
+
+    return {
+        next: function () {
+            return nextIndex < profiles.length
+                ? { value: profiles[nextIndex++], done: false }
+                : { done: true }
+        }
+    }
 }
